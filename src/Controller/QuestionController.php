f6a465ed3dc05fc8805dbaddc8f334400f0060e7
ammargomaa1 <ammargomaa1@gmail.com>
2022-09-06 06:33:47 +0200

    added debug pack and twig templates

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -1,15 +1,17 @@
 <?php
+
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
      */
-    public function homepage() : Response
+    public function homepage(): Response
     {
         return new Response('Hello From My first symfony route and controller');
     }
@@ -20,6 +22,15 @@ class QuestionController
 
      public function show($slug): Response
      {
-         return new Response('<h1>HI</h1> ' . $slug);
+         $answers = [
+                'Make sure your cat is sitting purrrfectly still ?',
+                'Honestly, I like furry shoes better than MY cat',
+
+                'Maybe... try saying the spell backwards?',
+             ];
+         return $this->render('question/show.html.twig', [
+             'question' => ucwords(str_replace('-', ' ', $slug)),
+             'answers' => $answers
+         ]);
      }
 }
